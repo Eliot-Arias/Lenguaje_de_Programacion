ebcead2f683d5a257113b137585085c49a859cef
eliot-arias <rampagecopilation@gmail.com>
2023-04-08 15:44:31 -0500

    Calculadora WPF

---
+++ taller8/CalculadoraWPF/MyTextBox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace CalculadoraWPF
+{
+    internal class MyTextBox : TextBox
+    {
+        protected override void OnPreviewGotKeyboardFocus(KeyboardFocusChangedEventArgs e)
+        {
+            e.Handled = true;
+            base.OnPreviewGotKeyboardFocus(e);
+        }
+
+    }
+}
